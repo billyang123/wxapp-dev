34f137c6114823ee7d2bba9270c290a67c83ab0a
杨斌斌 <yangbinbin@chinamuxie.com>
2016-12-26 13:16:55 +0800

    测试环境切换

--- src/app.js
+++ src/app.js
@@ -1,35 +1,50 @@
 import wx from 'labrador';
 import { sleep } from './utils/util';
-Date.prototype.format=function(fmt) {         
-    var o = {         
-    "M+" : this.getMonth()+1, //月份         
-    "d+" : this.getDate(), //日         
-    "h+" : this.getHours()%12 == 0 ? 12 : this.getHours()%12, //小时         
-    "H+" : this.getHours(), //小时         
-    "m+" : this.getMinutes(), //分         
-    "s+" : this.getSeconds(), //秒         
-    "q+" : Math.floor((this.getMonth()+3)/3), //季度         
-    "S" : this.getMilliseconds() //毫秒         
+Date.prototype.format=function(fmt) {
+    //yyyy-MM-dd hh:mm:ss       
+    // var o = {         
+    // "M+" : this.getMonth()+1, //月份         
+    // "d+" : this.getDate(), //日         
+    // "h+" : this.getHours()%12 == 0 ? 12 : this.getHours()%12, //小时         
+    // "H+" : this.getHours(), //小时         
+    // "m+" : this.getMinutes(), //分         
+    // "s+" : this.getSeconds(), //秒         
+    // "q+" : Math.floor((this.getMonth()+3)/3), //季度         
+    // "S" : this.getMilliseconds() //毫秒         
+    // }; 
+    var o = {
+    "yyyy": this.getFullYear(),      
+    "MM" : this.getMonth()+1, //月份
+    "dd" : this.getDate(), //日             
+    "HH" : this.getHours(), //小时         
+    "mm" : this.getMinutes(), //分         
+    "ss" : this.getSeconds()     
     };         
-    var week = {         
-    "0" : "/u65e5",         
-    "1" : "/u4e00",         
-    "2" : "/u4e8c",         
-    "3" : "/u4e09",         
-    "4" : "/u56db",         
-    "5" : "/u4e94",         
-    "6" : "/u516d"        
-    };         
-    if(/(y+)/.test(fmt)){         
-        fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));         
-    }         
-    if(/(E+)/.test(fmt)){         
-        fmt=fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? "/u661f/u671f" : "/u5468") : "")+week[this.getDay()+""]);         
-    }         
-    for(var k in o){         
-        if(new RegExp("("+ k +")").test(fmt)){         
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));         
-        }         
+    // var week = {         
+    // "0" : "/u65e5",         
+    // "1" : "/u4e00",         
+    // "2" : "/u4e8c",         
+    // "3" : "/u4e09",         
+    // "4" : "/u56db",         
+    // "5" : "/u4e94",         
+    // "6" : "/u516d"        
+    // };         
+    // if(/yyyy/.test(fmt)){         
+    //     fmt=fmt.replace("yyyy", this.getFullYear()+"");         
+    // }         
+    // if(/(E+)/.test(fmt)){         
+    //     fmt=fmt.replace(RegExp.$1, ((RegExp.$1.length>1) ? (RegExp.$1.length>2 ? "/u661f/u671f" : "/u5468") : "")+week[this.getDay()+""]);         
+    // }         
+    for(var k in o){
+        fmt = fmt.replace(k, o[k])
+        // let reg = new RegExp("("+ k +")");
+        // let rM = fmt.match(reg);
+        // if(rM){
+        //   fmt = fmt.replace(rM[1], o[k])
+        // }
+        // if(new RegExp("("+ k +")").test(fmt)){         
+        //     fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));         
+        // }         
     }         
     return fmt;         
 }
@@ -40,8 +55,8 @@ export default class {
   };
   data = {
     assetsPath:'https://s1.chinamuxie.com/www/assets/xcx',
-    //ajaxPath:'https://xcx.chinamuxie.com'//'https://xcx.yiqihuzhu.com/',
-    ajaxPath:'https://xcx.yiqihuzhu.com'//'https://xcx.yiqihuzhu.com/',
+    ajaxPath:'https://xcx.chinamuxie.com'//'https://xcx.yiqihuzhu.com/',
+    //ajaxPath:'https://xcx.yiqihuzhu.com'//'https://xcx.yiqihuzhu.com/',
   };
   async onLaunch() {
     var _this = this;
